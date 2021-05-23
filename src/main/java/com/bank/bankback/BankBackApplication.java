f8b968c5a7e5e30ffc23b1a81f06cb446db3c9ec
Alejandra Gonzalez <aleja.gonzalezv19@gmail.com>
2021-05-22 19:21:32 -0500

    Cors added

--- src/main/java/com/bank/bankback/BankBackApplication.java
+++ src/main/java/com/bank/bankback/BankBackApplication.java
@@ -1,13 +1,34 @@
 package com.bank.bankback;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BankBackApplication {
 
+    @Value("${ALLOWED_ORIGINS:http://localhost:4200}")
+    private String allowedOrigins;
+
     public static void main(String[] args) {
         SpringApplication.run(BankBackApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins(allowedOrigins)
+                        .allowedMethods("*");
+            }
+        };
     }
 
 }
