46b36217ef8ac114602d517f4c691fa40b6e2539
Alejandra Gonzalez <aleja.gonzalezv19@gmail.com>
2021-05-23 16:45:11 -0500

    Model changes

--- src/main/java/com/bank/bankback/BankBackApplication.java
+++ src/main/java/com/bank/bankback/BankBackApplication.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@CrossOrigin(origins="*")
 @SpringBootApplication
 public class BankBackApplication {
 
