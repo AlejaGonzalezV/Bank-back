635c4d35be7a036acc2b72567ed24c16c417b95f
Alejandra Gonzalez <aleja.gonzalezv19@gmail.com>
2021-05-23 15:39:23 -0500

    Cors changes

--- src/main/java/com/bank/bankback/BankBackApplication.java
+++ src/main/java/com/bank/bankback/BankBackApplication.java
@@ -5,13 +5,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@CrossOrigin(origins="*")
 @SpringBootApplication
 public class BankBackApplication {
 
-    @Value("${ALLOWED_ORIGINS:*}")
+    @Value("${ALLOWED_ORIGINS:http://localhost:8080}")
     private String allowedOrigins;
 
     public static void main(String[] args) {
