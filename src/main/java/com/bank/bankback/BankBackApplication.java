af3faadc8e975d13f3e5a4d478696cdcc09f6cbc
Alejandra Gonzalez <aleja.gonzalezv19@gmail.com>
2021-05-23 16:59:32 -0500

    Controller changes

--- src/main/java/com/bank/bankback/BankBackApplication.java
+++ src/main/java/com/bank/bankback/BankBackApplication.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class BankBackApplication {
 
-    @Value("${ALLOWED_ORIGINS:http://localhost:8080}")
+    //@Value("${ALLOWED_ORIGINS:http://localhost:8080}")
     private String allowedOrigins;
 
     public static void main(String[] args) {
